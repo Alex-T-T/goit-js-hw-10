7214fac9bb00d7df2c72aa0db84c1d229b34d75d
Olexandr Tolochko <0504fusion@gmail.com>
2022-08-02 22:35:26 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,3 +3,14 @@ import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+
+const input = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+input.addEventListener('input', debounce(fetchCountries(), DEBOUNCE_DELAY));
+
+function fetchCountries(name) {
+    
+}
+
