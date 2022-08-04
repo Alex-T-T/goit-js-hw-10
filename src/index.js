73d5dd5f0b2ceaaa686dfc04a6efbe27b2cf5575
Olexandr Tolochko <0504fusion@gmail.com>
2022-08-04 18:07:55 +0300

    done but need some actions more

--- src/index.js
+++ src/index.js
@@ -1,50 +1,83 @@
+// ==============================
+// imports libraries
+
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 
+// ==============================
+// imporn fetchCoutries function
+
+import { fetchCountries } from './fetchCountries'
+
+// ==============================
+
 const DEBOUNCE_DELAY = 300;
+
+// ==============================
+// work with DOM
 
 const input = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
 
+// ==============================
+// add event listener
+
 input.addEventListener('input', debounce(searchCountries, DEBOUNCE_DELAY));
+
+// ==============================
+// search input value
 
 function searchCountries(event) {
     event.preventDefault();
 
     const searchValue = input.value;
-    console.log(searchValue);
+    let trimedSearchValue = searchValue.trim();
 
-    fetchCountries(searchValue)
+    if (trimedSearchValue === '') {
+
+        removeHTML();
+
+        Notiflix.Notify.info('Please enter a name.');
+    return
+    }
+
+    fetchCountries(trimedSearchValue)
     .then(renderCountry)
-    .catch(ifFetchError)
-    .finally(() => input.value = '');
+    .catch(ifFetchError);
 }
 
-function fetchCountries(value) {
-    const url = `https://restcountries.com/v3.1/name/${value}?fields=name,capital,flag,population,languages`;
-
-    return fetch(url).then(response => response.json());
-}
+// ==============================
+// render HTML
 
 function renderCountry(countries) {
-    const size = Object.keys(countries).length;
     
-    countryList.innerHTML = '';
-    countryInfo.innerHTML = '';
-  
-    console.log(size)
+    const size = Object.keys(countries).length;
+
+    removeHTML();
+
     for (const country of countries) {
 
-        const {flag, name, capital, population, languages} = country
-
-        const elementSymbols = `<li><h2 class="country-mame">${flag} ${name.official}</h2></li>`
         
-        const elementInfo = `<p class="counry-capital">Capital: ${capital}</p>
-        <p class="counry-population">Population: ${population}</p>
-        <p class="counry-laguages">Languages: ${Object.values(languages)}</p>`
+        const { flags, name, capital, population, languages } = country;
 
+        const elementSymbols = `<li class = 'country-item'> <a class = 'country-flag' href = '${flags.svg}'>
+        <img src = '${flags.svg}' alt = '${name.official}' width = '30' > </a>
+        <h2 class="country-name">  ${name.official}</h2>
+        </li>`;
+        
+        const elementInfo = `<p class="counry-capital"> <b>Capital:</b> <i>${capital}</i></p>
+        <p class="counry-population"><b>Population:</b> <i>${population}</i></p>
+        <p class="counry-laguages"><b>Languages:</b> <i>${Object.values(languages)}</i></p>`;
+
+        // checkSize(size, elementInfo, elementSymbols);
+
+        if (size > 10) {
+            // Notiflix.Notify.info('Too many matches found. Please enter a more specific name.')
+            console.log('add more letters')
+            return
+        }
 
         if (size <= 1) {
             countryList.insertAdjacentHTML('beforeend', elementSymbols);
@@ -53,18 +86,42 @@ function renderCountry(countries) {
         else {
             countryList.insertAdjacentHTML('beforeend', elementSymbols);
         }
-        
     }
 }
 
-// function countryCard() {
-//     return `<h2 class="country-mame">${name.official}</h2>
-//       <p class="counry-capital">Capital: ${capital}</p>
-//       <p class="counry-population">Population: {{population}}</p>
-//       <p class="counry-laguages">Laguages: {{laguages}}</p>`
+// ==============================
+// check size answer object 
+
+// function checkSize(size, elementInfo, elementSymbols) {
+
+//     // const size = Object.keys(countries).length;
+
+//         if (size > 10) {
+//             // Notiflix.Notify.info('Too many matches found. Please enter a more specific name.')
+//             console.log('add more letters')
+//             return
+//         }
+
+//         if (size <= 1) {
+//             countryList.insertAdjacentHTML('beforeend', elementSymbols);
+//             countryInfo.insertAdjacentHTML('beforeend', elementInfo);
+//         }
+//         else {
+//             countryList.insertAdjacentHTML('beforeend', elementSymbols);
+//         }
 // }
+
+// ==============================
+// fetch error
 
 function ifFetchError(error) {
     Notiflix.Notify.failure('Oops, there is no country with that name')
 }
 
+// ==============================
+// remove HTML before rendering
+
+function removeHTML () {
+    countryList.innerHTML = '';
+    countryInfo.innerHTML = '';
+}
